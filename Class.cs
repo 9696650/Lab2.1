f6228771d7113455c1c3c5c1c9a485c0f43241e3
Xxy8gxiydy8d8ydy8dxyixtixtixoyxy8x8ycyociycidyic8yxitxtixiyxyxyixyix <9696650@stud.kai.edu.ua>
2026-05-06 21:48:57 +0300

    Update and rename Class.ch to Class.cs

---
+++ Class.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._4
+{
+    public class Figure
+    {
+
+        private double sideX;
+        private double sideY;
+
+
+        public Figure()
+        {
+            sideX = 0;
+            sideY = 0;
+        }
+
+
+        public Figure(double x, double y)
+        {
+            sideX = x;
+            sideY = y;
+        }
+
+
+        public double SideX => sideX;
+        public double SideY => sideY;
+
+
+        public double SideLength()
+        {
+
+            return Math.Sqrt(sideX * sideX + sideY * sideY);
+        }
+    }
+    public class Rhombus : Figure
+    {
+        private double height;
+
+
+        public Rhombus() : base()
+        {
+            height = 0;
+        }
+
+
+        public Rhombus(double x, double y, double h) : base(x, y)
+        {
+            height = h;
+        }
+
+
+        public double Height => height;
+
+
+        public double GetPerimeter()
+        {
+
+            return 4 * SideLength();
+        }
+
+
+        public double GetArea()
+        {
+            double side = SideLength();
+            return side * height;
+        }
+
+
+        public string GetInfo()
+        {
+            return $"Info: Side = {SideLength():F2}, Height = {height:F2}";
+        }
+    }
+}
